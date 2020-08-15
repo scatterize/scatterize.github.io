0f0c10f8be80b8cbc1392e0f804ebe3853ab3882
scatterize <scatterize@localhost.localdomain>
2020-08-15 05:58:55 -0400

    first commit

---
+++ precache-manifest.29b261463bd973b337903848661e2911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9982cd7baa05f3b18e2c755ddc15cee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8819063c6d39b445ec60",
+    "url": "/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "e40e0289c974f89b005d",
+    "url": "/static/js/2.f543ed19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f543ed19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8819063c6d39b445ec60",
+    "url": "/static/js/main.444b4962.chunk.js"
+  },
+  {
+    "revision": "1906aaf2a58ce974dcf5",
+    "url": "/static/js/runtime-main.b30c7ae7.js"
+  }
+]);
